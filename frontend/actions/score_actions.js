592564d772b08a3afa5817274b004f863b1db277
Julia <juliawang105@gmail.com>
2019-12-02 10:20:41 -0800

    create scores actions

--- frontend/actions/score_actions.js
+++ frontend/actions/score_actions.js
@@ -1,3 +1,18 @@
 import * as ScoreUtil from '../util/score_util';
 
-export const 
+export const RECEIVE_SCORE = 'RECEIVE_SCORE';
+
+const receiveScore = score => ({
+    type: RECEIVE_SCORE,
+    score
+});
+
+export const fetchScore = scoreId => dispatch => (
+    ScoreUtil.fetchScore(scoreId)
+        .then(res => dispatch(receiveScore(res)))
+);
+
+export const updateScore = score => dispatch => (
+    ScoreUtil.updateScore(score)
+        .then(res => dispatch(receiveScore(res)))
+);
