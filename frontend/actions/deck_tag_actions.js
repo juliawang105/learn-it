628579bcbc4d8ff69c0374b3fc27593b1dd86b0b
Julia <juliawang105@gmail.com>
2019-12-10 09:54:42 -0800

    finish deckTag actions

---
+++ frontend/actions/deck_tag_actions.js
@@ -0,0 +1,31 @@
+import * as DeckTagUtil from '../util/deck_tag_util';
+
+
+//export const RECEIVE_ALL_DECK_TAGS = " RECEIVE_ALL_DECK_TAGS";
+export const RECEIVE_DECK_TAG = "RECEIVE_DECK_TAG";
+export const REMOVE_DECK_TAG = "REMOVE_DECK_TAG"
+
+// const receiveAllDeckTags = deckTags => ({
+//     type: RECEIVE_ALL_TAGS,
+//     deckTags
+// });
+
+const receiveDeckTag = deckTag => ({
+    type: RECEIVE_DECK_TAG,
+    deckTag
+});
+
+const removeDeckTag = deckTagId => ({
+    type: REMOVE_DECK_TAG,
+    deckTagId
+})
+
+export const createDeckTag = deckTag => dispatch (
+    DeckTagUtil.createDeckTag(deckTag)
+        .then( res => dispatch(receiveDeckTag(res)))
+);
+
+export const deleteDeckTag = deckTagId => dispatch(
+    DeckTagUtil.deleteDeckTag(deckTagId)
+        .then( () => dispatch(removeDeckTag(deckTagId)))
+);
