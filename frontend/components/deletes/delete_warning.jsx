5b8d5314e3d34c230d028bfb391133844a668190
Julia <juliawang105@gmail.com>
2019-12-13 09:52:02 -0800

    fix deletion

--- frontend/components/deletes/delete_warning.jsx
+++ frontend/components/deletes/delete_warning.jsx
@@ -24,10 +24,19 @@ class DeleteWarning extends React.Component{
 
     handleDelete(){
         event.preventDefault();
-        this.props.deleteCard(this.props.data)
-            .then(() => {
-                this.props.closeModal()
-            })       
+        debugger
+        if(Object.keys(this.props.cards).length === 0){
+            this.props.deleteDeck(this.props.data)
+                .then(() => {
+                    this.props.closeModal()
+                })     
+        } else {
+            this.props.deleteCard(this.props.data)
+                .then(() => {
+                    this.props.closeModal()
+                }) 
+        }
+          
     }
 
     render(){
@@ -35,7 +44,7 @@ class DeleteWarning extends React.Component{
         return(
             <div className="delete">
                 <div tabIndex="0" onKeyDown={this.escFunction}></div>
-                Are you sure you want to delete this deck?
+                Are you sure you want to delete this?
                  <div onClick={this.props.closeModal} className="close-x-login">×</div>
                 <div className="double-check">
                     <button onClick={this.handleDelete}>Yes, Please Delete</button>
