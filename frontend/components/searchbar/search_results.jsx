a54c1e223c673b656c8eecf20ec9d9f5173595b5
Julia <juliawang105@gmail.com>
2019-12-14 19:19:44 -0800

    render correct decks on search container

--- frontend/components/searchbar/search_results.jsx
+++ frontend/components/searchbar/search_results.jsx
@@ -1,20 +1,63 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-
+import DeckItem from '../decks/deck_index_item';
 class SearchResults extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
+
+        this.state = {
+            decks: []
+        };
+        
     };
 
     componentDidMount(){
-        if(this.props.history.location)
+        let ids = this.props.history.location.search.slice(5);
+        let idArr = ids.split(",").map( id => {
+            return parseInt(id)
+        });
+        
+        let result = [];
+
+        for(let i = 0; i < idArr.length; i ++ ){
+            this.props.fetchDeck(idArr[i])
+            .then( res =>{
+                result.push(res.payload.deck)
+                this.setState({ decks: result })
+            })
+
+        }
     }
 
+    componentDidUpdate(oldProps){
+        if(oldProps.history.location.search !== this.props.history.location.search){
+            let ids = this.props.history.location.search.slice(5);
+            let idArr = ids.split(",").map(id => {
+                return parseInt(id)
+            });
+
+            let result = [];
+
+            for (let i = 0; i < idArr.length; i++) {
+                this.props.fetchDeck(idArr[i])
+                    .then(res => {
+                        result.push(res.payload.deck)
+                        this.setState({ decks: result })
+                 })
+            };
+        };
+    };
+
     render(){
-        debugger
+       
+        if(!this.state.decks) return null; 
+        let matches = this.state.decks.map( deck => {
+            return <DeckItem key={deck.id} deck={deck} />
+        })
+        //debugger
         return(
             <div>
-                Hello 
+                {matches}
             </div>
         )
     }
