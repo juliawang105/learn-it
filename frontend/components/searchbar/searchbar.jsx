2429c1e106b77de079f360e643e473a7c80631c7
Julia <juliawang105@gmail.com>
2019-12-14 17:04:08 -0800

    allow enter key to search for decks

--- frontend/components/searchbar/searchbar.jsx
+++ frontend/components/searchbar/searchbar.jsx
@@ -2,13 +2,47 @@ import React from 'react';
 
 class SearchBar extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
+
+        this.state = {
+            input: ""
+        }
+
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.enterFunction = this.enterFunction.bind(this);
     };
 
+    componentDidMount(){
+        document.addEventListener('keydown', this.enterFunction)
+    }
+
+    handleInput(){
+        event.preventDefault();
+        this.setState({input: event.target.value})
+    }
+
+    enterFunction(e) {
+        // e.preventDefault();
+        if (e.keyCode === 13) {
+            this.handleSearch()
+        };
+    };
+
+
+    handleSearch(){
+        this.props.search(this.state.input)
+            .then( res => {
+                console.log(res)
+            })
+    }
+
     render(){
+       //debugger
         return(
             <div className="searchbar">
-                <input type="text" name="" id="" placeholder='Search by Deck Names'/>
+                <div tabIndex="0" onKeyDown={this.enterFunction}></div>
+                <input onChange={this.handleInput} type="text" value={this.state.input}placeholder='Search by Deck Names'/>
             </div>
         )
     };
